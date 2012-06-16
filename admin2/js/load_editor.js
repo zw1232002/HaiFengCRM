b162bf941c048aecdea36d8cad52c0f3204ed27a
MaLucheng <mlc880926@gmail.com>
2012-06-16 13:29:17 +0800

    add course kindeditor

--- admin2/js/load_editor.js
+++ admin2/js/load_editor.js
@@ -1,6 +1,6 @@
 	//kindedit	
     KindEditor.ready(function(K) {
-            var editor = K.create('#class_description',{
+            var editor = K.create('.class_description',{
             	width : '704px',
             	height:"400px",
             	resizeType:0
